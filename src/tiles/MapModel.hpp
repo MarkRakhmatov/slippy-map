b7618f426234171fa768a7e0d151bf01ee770419
Mark <mark.rakhmatov@gmail.com>
2025-04-05 00:06:17 +0300

    code refactoring, fix tests

--- src/tiles/MapModel.hpp
+++ src/tiles/MapModel.hpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <cstdint>
 #include <functional>
+#include <optional>
 #include <unordered_map>
 #include <vector>
 
@@ -10,6 +11,11 @@
 #include "Utils.hpp"
 
 namespace geo {
+using int_range = std::tuple<int, int>;
+using optional_range = std::optional<int_range>;
+using optional_ranges =
+    std::tuple<std::optional<int_range>, std::optional<int_range>>;
+
 struct TilesBounds {
   int32_t minx;
   int32_t miny;
@@ -18,55 +24,34 @@ struct TilesBounds {
   int32_t z;
 
   auto contains(const geo::Tile &tile) const -> bool {
-    auto [minx1, maxx] = getMinMaxX();
-    auto [miny1, maxy] = getMinMaxY();
-    return tile.z == z && tile.x >= minx1 && tile.x <= maxx &&
-           tile.y >= miny1 && tile.y <= maxy;
-  }
-
-  auto getMinMaxX() const -> std::tuple<int, int> {
-    if (z == 0) {
-      return {0, 0};
+    if (tile.z != z) {
+      return false;
     }
-    if (z == 1) {
-      return {0, 1};
-    }
-    if (minx + xsize >= 1 << z) {
-      return {0, (1 << z) - 1};
-    }
-    auto minx1 = mod(minx, 1 << z);
-    int32_t maxx = mod(minx1 + xsize, 1 << z);
-    if (maxx < minx1) {
-      std::swap(maxx, minx1);
-    }
-    return {minx1, maxx};
-  }
-
-  auto getMinMaxY() const -> std::tuple<int, int> {
-    if (z == 0) {
-      return {0, 0};
-    }
-    if (z == 1) {
-      return {0, 1};
-    }
-    if (miny + ysize >= 1 << z) {
-      return {0, (1 << z) - 1};
-    }
-    auto miny1 = mod(miny, 1 << z);
-    int32_t maxy = mod(miny1 + ysize, 1 << z);
-    if (maxy < miny1) {
-      std::swap(maxy, miny1);
-    }
-    return {miny1, maxy};
+    auto [xrange1, xrange2] = getXRanges();
+    auto [yrange1, yrange2] = getYRanges();
+    bool containsX = inRange(tile.x, xrange1.value()) ||
+                     (xrange2 && inRange(tile.x, xrange2.value()));
+    bool containsY = inRange(tile.y, yrange1.value()) ||
+                     (yrange2 && inRange(tile.y, yrange2.value()));
+    return containsX && containsY;
   }
 
   void iterateUniqueTiles(
       std::function<void(const geo::Tile &)> tileCB) const noexcept {
-    auto [minx1, maxx] = getMinMaxX();
-    auto [miny1, maxy] = getMinMaxY();
+    int32_t maxX = 1 << z;
+    int32_t maxY = 1 << z;
+
+    auto minx1 = mod(minx, maxX);
+    auto xsizeRounded = std::min(xsize, maxX - 1);
+    int32_t maxx = minx1 + xsizeRounded;
+
+    auto miny1 = mod(miny, maxY);
+    auto ysizeRounded = std::min(ysize, maxY - 1);
+    int32_t maxy = miny1 + ysizeRounded;
+
     for (int32_t x = minx1; x <= maxx; ++x) {
       for (int32_t y = miny1; y <= maxy; ++y) {
-        tileCB(geo::Tile{x, y, z});
+        tileCB(geo::Tile{mod(x, maxX), mod(y, maxY), z});
       }
     }
   }
@@ -78,6 +63,49 @@ struct TilesBounds {
         tileCB({x, y, z});
       }
     }
+  }
+
+private:
+  auto inRange(int value, const int_range &r) const -> bool {
+    return value >= std::get<0>(r) && value <= std::get<1>(r);
+  }
+
+  auto getXRanges() const -> optional_ranges {
+    if (z == 0) {
+      return optional_ranges{int_range{0, 0}, std::nullopt};
+    }
+    if (z == 1) {
+      return optional_ranges{int_range{0, 1}, std::nullopt};
+    }
+    const int maxXSize = static_cast<int>(1 << z);
+    if (xsize >= maxXSize) {
+      return optional_ranges{int_range{0, maxXSize - 1}, std::nullopt};
+    }
+    int32_t minx1 = mod(minx, maxXSize);
+    int32_t maxx = mod(minx1 + xsize, maxXSize);
+    if (minx <= maxx) {
+      return optional_ranges{int_range{minx1, maxx}, std::nullopt};
+    }
+    return optional_ranges{int_range{0, maxx}, int_range{minx1, maxXSize - 1}};
+  }
+
+  auto getYRanges() const -> optional_ranges {
+    if (z == 0) {
+      return optional_ranges{int_range{0, 0}, std::nullopt};
+    }
+    if (z == 1) {
+      return optional_ranges{int_range{0, 1}, std::nullopt};
+    }
+    const int maxYSize = static_cast<int>(1 << z);
+    if (ysize >= maxYSize) {
+      return optional_ranges{int_range{0, maxYSize - 1}, std::nullopt};
+    }
+    int32_t miny1 = mod(miny, maxYSize);
+    int32_t maxy = mod(miny1 + ysize, maxYSize);
+    if (miny1 <= maxy) {
+      return optional_ranges{int_range{miny1, maxy}, std::nullopt};
+    }
+    return optional_ranges{int_range{0, maxy}, int_range{miny1, maxYSize}};
   }
 };
 
